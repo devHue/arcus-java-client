64287354c8a1e5532011cce78e707e44f909a3ca
devHue <developerhue@gmail.com>
2017-08-18 19:11:40 +0900

    Revert "FEATURE: Add mget, mgets"
    This reverts commit c72f54f361b63d09154ebe531f65fa6dfd04366f.

--- src/main/java/net/spy/memcached/protocol/binary/BinaryOperationFactory.java
+++ src/main/java/net/spy/memcached/protocol/binary/BinaryOperationFactory.java
@@ -89,7 +89,6 @@ import net.spy.memcached.ops.StoreOperation;
 import net.spy.memcached.ops.StoreType;
 import net.spy.memcached.ops.VersionOperation;
 
-
 /**
  * Factory for binary operations.
  */
@@ -115,17 +114,6 @@ public class BinaryOperationFactory extends BaseOperationFactory {
 	public GetsOperation gets(String key, GetsOperation.Callback cb) {
 		return new GetOperationImpl(key, cb);
 	}
-
-	public GetOperation mget(Collection<String> keys, GetOperation.Callback cb) {
-		throw new RuntimeException(
-				"mget is not supported in binary protocol yet.");
-	}
-
-	public GetsOperation mgets(String key, GetsOperation.Callback cb) {
-		throw new RuntimeException(
-				"mgets is not supported in binary protocol yet.");
-	}
-
 
 	public MutatorOperation mutate(Mutator m, String key, int by,
 			long def, int exp, OperationCallback cb) {
